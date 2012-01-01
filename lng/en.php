91a37ee355cc48123f28b1ddb7674f903064a251
gH0St Arthour <ghostarthour@gmail.com>
2012-01-01 11:52:58 +0100

    Added multiple language support

---
+++ lng/en.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+	"name"=>"Name",
+	"summary"=>"Summary",
+	"state"=>"State",
+	"down_speed"=>"Down Speed",
+	"up_speed"=>"Upload Speed",
+	"peers"=>"Peers",
+	"downloaded"=>"Downloaded",
+	"uploaded"=>"Uploaded",
+	"full_size"=>"Full size",
+	"control"=>"CONTROL",
+	"uploading"=>"Uploading",
+	"downloading"=>"Downloading",
+	"starting"=>"Starting...",
+	"stopping"=>"Stopping...",
+	"start"=>"START",
+	"stop"=>"STOP",
+	"delete"=>"DELETE",
+	"free"=>"Free",
+	"used"=>"Used",
+	"total"=>"Total",
+	"loading"=>"Loading...",
+	"confirm_delete"=>"Do you really want to delete?"
+);
+
---
+++ lng/hu.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+	"name"=>"Név",
+	"summary"=>"Öszegzés",
+	"state"=>"Állapot",
+	"down_speed"=>"Letöltés",
+	"up_speed"=>"Feltöltés",
+	"peers"=>"Csatlakozók",
+	"downloaded"=>"Letöltve",
+	"uploaded"=>"Feltöltve",
+	"full_size"=>"Teljes méret",
+	"control"=>"IRÁNYÍTÁS",
+	"uploading"=>"Feltöltés",
+	"downloading"=>"Letöltés",
+	"starting"=>"Indítás...",
+	"stopping"=>"Leállítás...",
+	"start"=>"INDÍTÁS",
+	"stop"=>"LEÁLLÍTÁS",
+	"delete"=>"TÖRLÉS",
+	"free"=>"Szabad",
+	"used"=>"Használt",
+	"total"=>"Teljes",
+	"loading"=>"Töltés...",
+	"confirm_delete"=>"Valóban törölni szeretnéd a torrentet?"
+);
+
